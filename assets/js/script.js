c7608d2a0463a8cdd75040f2dc97e8ea27f2d2cb
chloë js <chloejs.horta@gmail.com>
2022-06-02 15:22:03 +0000

    add error status and pop up modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,9 +13,21 @@ async function getStatus(e) {
     const data = await response.json();
 
     if (response.ok) {
-        console.log(data.expiry);
+        displayStatus(data);
     } else {
-        console.log('ERR');
+        throw new Error(data.error);
     }
+
+}
+
+function displayStatus(data) {
+
+    let heading = "API Key Status";
+    let results = `<div>Your key is valid until</div>`;
+    results += `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
 
 }
